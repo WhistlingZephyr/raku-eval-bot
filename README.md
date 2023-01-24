8fbd69cfa17fc2b94156c978263d19ae04d63a46
WhistlingZephyr <90854121+WhistlingZephyr@users.noreply.github.com>
2023-01-24 18:39:12 +0530

    feat: first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# A discord eval bot for Raku
+
+Use `m: <code>` to eval Raku code. It supports inline and multiline code blocks
+
+## Setup
+
+### Environment
+
+Create an `.env` file with `API_KEY` and `DISCORD_TOKEN`. You can get your glot.io API key from <https://glot.io/account/token>
+
+### Run
+
+Run the bot with
+
+```sh
+deno run --allow-env --allow-read --allow-net app.ts
+```
---
+++ app.ts
@@ -0,0 +1,53 @@
+import {
+  createBot,
+  Intents,
+  startBot,
+} from "https://deno.land/x/discordeno@13.0.0/mod.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+const bot = createBot({
+  token: Deno.env.get("DISCORD_TOKEN")!,
+  intents: Intents.GuildMessages | Intents.MessageContent,
+  events: {
+    ready(_bot, payload) {
+      console.log(
+        `Successfully connected to gateway as ${payload.user.id} (${payload.user.username}#${payload.user.discriminator})`,
+      );
+    },
+    async messageCreate(bot, message) {
+      if (message.content.startsWith("m:")) {
+        const code = message.content.match(
+          /m:\s*(?:```(?:\S*\n)?(.+?)```|`(.+?)`|(.+))/s,
+        )
+          ?.slice(1, 4).find(Boolean);
+        if (code) {
+          const result: { stdout: string; stderr: string; error: string } =
+            await (await fetch("https://glot.io/api/run/raku/latest", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: Deno.env.get("API_KEY") ?? "",
+              },
+              body: JSON.stringify({
+                files: [{ name: "main.raku", content: code }],
+              }),
+            })).json();
+          bot.helpers.sendMessage(
+            message.channelId,
+            {
+              content: `\`\`\`\n${
+                Object.values(result).filter(Boolean).join("\n")
+              }\n\`\`\``,
+              messageReference: {
+                messageId: message.id,
+                failIfNotExists: false,
+              },
+            },
+          );
+        }
+      }
+    },
+  },
+});
+
+await startBot(bot);
